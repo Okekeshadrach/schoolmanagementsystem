d785ac30ad94f9ef851d937fe978e28b16f1c76b
SonJayDev-Techs <105818488+SonJayDev-Techs@users.noreply.github.com>
2022-05-25 22:22:12 +0100

    Update index.php

--- stmonica/index.php
+++ stmonica/index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- <meta http-equiv="content-type" content="text/html;charset=UTF-8" /> -->
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
